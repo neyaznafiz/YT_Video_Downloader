dea9f6bb33dd237db9d2734bd792b1342ee95870
neyaznafiz <neyaznafiz@outlook.com>
2023-03-16 01:51:30 +0600

    get video info function and api impliment in backend

--- Front-end/src/Cmponents/Download.jsx
+++ Front-end/src/Cmponents/Download.jsx
@@ -1,14 +1,30 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const Download = () => {
   const [link, setLink] = useState("");
-  const [vdInfo, setVdInfo]= useState('')
+  const [vdInfo, setVdInfo] = useState("");
 
+  //   for get input valu with onChange
   const handleChange = (e) => {
-    e.preventDefault();
     setLink(e.target.value);
   };
 
+  // form submit
+  const videoDetailsGet = async (e) => {
+    e.preventDefault();
+
+    const videoIdOne = link.split("https://youtu.be/")[1];
+    const videoIdTwo = link.split("https://www.youtube.com/watch?v=")[1];
+
+    try {
+      const { data } = await axios.get(`http://localhost:5000/api/get-video-info/${videoIdOne || videoIdTwo}`);
+
+      console.log(data)
+    } catch (error) {
+     console.log(error.response)   
+    }
+  };
 
   return (
     <div className="w-[600px] h-[500px] bg-gray-900 flex flex-col justify-start items-center p-4 relative">
@@ -16,15 +32,18 @@ const Download = () => {
 
       {/* input section */}
       <div className="mt-8">
-        <form>
+        <form onSubmit={videoDetailsGet}>
           <input
+            onChange={handleChange}
+            required
             type="text"
             placeholder="Input the video link here"
-            onChange={handleChange}
             className="text-white outline-none bg-transparent w-[380px] h-[40px] px-3 overflow-hidden border border-yellow-600"
           />
 
-          <button className="button animation">Click</button>
+          <button type="submit" className="button animation">
+            Click
+          </button>
         </form>
       </div>
     </div>
--- server.js
+++ server.js
@@ -1,17 +1,24 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const ytdl = require('ytdl-core')
 
-const port = 5000
+const port = 5000;
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
+// get video info api
+app.get("/api/get-video-info/:videoId", async (req, res) => {
+  const { videoId } = req.params;
+  const {videoDetails, formats} = await ytdl.getInfo(videoId)
+  console.log(formats)
+});
 
-app.get('/', (req,res) => {
-    res.send('Hello Downloader')
-})
+app.get("/", (req, res) => {
+  res.send("Hello Downloader");
+});
 
-app.listen(port, ()=> {
-    console.log(`Downloader app server is running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Downloader app server is running on port ${port}`);
+});
